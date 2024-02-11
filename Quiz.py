6fd4f33ef235286c9a75ba1c527a10164e6481f7
jason5793 <jasonvlog81@gmail.com>
2024-02-11 21:18:37 +0530

    Adding question

---
+++ Quiz.py
@@ -0,0 +1,11 @@
+question=("how many elements are therein the periodic table ?",
+          "",
+          "",
+          "",
+          "",
+          "")
+option=((),(),(),(),())
+answer=()
+guesses=[]
+score=0
+question_number=0
